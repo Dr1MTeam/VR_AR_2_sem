91e735a5a96ef5c42ed0b51a8bb3f6d023df671c
Pesternikov Danil <danils003@mail.ru>
2024-06-08 03:21:55 +0300

    add: location and sound effects

---
+++ Assets/Trigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    
+    private AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        BoxCollider boxCollider = GetComponent<BoxCollider>();
+        boxCollider.isTrigger = true;
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+}
